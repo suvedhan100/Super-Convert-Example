3f75e3c5f40eed13dd3a12382140aac5f3465a77
suvedhan100 <suvedhan@live.com>
2022-12-02 12:31:30 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//using SuperConvert.Extensions;
+//using SuperConvert.Files;
+using SuperConvert.Files.Extensions;
+
+namespace Super_Convert_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string jsonFromCsv = ExcelConverter.CsvToJson("D:\\Projects\\Superconvertexample\\Super-Convert-Example\\sample.csv");
+            
+
+            Console.WriteLine(jsonFromCsv);
+        }
+    }
+}
